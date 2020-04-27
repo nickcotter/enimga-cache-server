b7cd45b7ba49eeeef4607c5ca56300acbe8be7ea
Nick Cotter <nick@extropy.net>
2020-04-27 16:36:44 +0100

    Dockerised version

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run --pid=host -p 8080:8080 enigma-cache-server
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
-// app.get('/', (req, res) => res.send('Hello World!'))
+const port = 8080
 
 app.use('/', express.static('static'))
 
